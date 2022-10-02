f7e1dc6194f6b6588650ff4807d27fbe4f283ac4
babymariobebe <christiandcamp@hotmail.com>
2022-10-01 20:13:02 -0500

    Started Recipe

--- front-end/src/components/Instances/Recipe.js
+++ front-end/src/components/Instances/Recipe.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import Recipes from '../Models/Recipes';
+import { useParams } from 'react-router-dom';
+//import './Instances.css'
 
 function Recipe() {
+    let { id } = useParams();
+    
     return (
       <div className="recipe">Recipe</div>
     );
