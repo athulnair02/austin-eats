048c979a0b15ab28d933d1c39a33999c269eb96e
babymariobebe <christiandcamp@hotmail.com>
2022-10-03 16:26:32 -0500

    Recipes & Recipe instance static

--- front-end/src/components/Instances/Recipe.js
+++ front-end/src/components/Instances/Recipe.js
@@ -1,13 +1,81 @@
 import React from 'react';
 import Recipes from '../Models/Recipes';
 import { useParams } from 'react-router-dom';
-//import './Instances.css'
+import './Instances.css'
+
+function RecipeInfoCell(props) {
+  return (
+    <div className='recipeInfoCell'>
+      <div className='recipeInfoTitle'>
+        {props.metric}
+      </div>
+      <div className='recipeInfoText'>
+        {props.value}
+      </div>
+    </div>
+  )
+}
 
 function Recipe() {
     let { id } = useParams();
     
+    const metrics = {
+      ['prep time']: '45 mins',
+      ['servings']: '3 servings',
+    }
+
+    const infoCells = [];
+    for (const metric in metrics) {
+      infoCells.push(<RecipeInfoCell metric={metric.toUpperCase()} value={metrics[metric]}/>)
+    }
+
     return (
-      <div className="recipe">Recipe</div>
+      <>
+        <div className='recipeTitle'>Fish Filet</div>
+        <div className='recipeImage'></div>
+        <div style={{paddingLeft: '50px', paddingRight: '50px'}}>
+          <div className='headerFrame'>
+            <div className='recipeSubtitle'>Recipe</div>
+            <div className='recipeInfoList'>
+              {infoCells}
+            </div>
+          </div>
+        </div>
+        <div className='recipeSubtitle'>Ingredients</div>
+        <ul className='horizontalBulletList'>
+          <li>2 Eggs</li>
+          <li>1 Quart Milk</li>
+          <li>2 Cups Flour</li>
+        </ul>
+        <div className='recipeSubtitle'>Preparation</div>
+        <ul className='horizontalNumberList'>
+          <li>Some super long and detailed step about preparing the ingredients for the dish</li>
+          <li>1 Quart Milk</li>
+        </ul>
+        <div className='recipeSubtitle'>Nutrition</div>
+        <table className='recipeTable'>
+          <tr>
+            <th>test</th>
+            <th>test2</th>
+            <th>test3</th>
+          </tr>
+          <tr>
+            <td>testasdsads</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+          <tr>
+            <td>testasdsads</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+          <tr>
+            <td>testasdsads</td>
+            <td>test</td>
+            <td>test</td>
+          </tr>
+        </table>
+      </>
     );
   }
   
--- front-end/src/components/Models/Recipes.js
+++ front-end/src/components/Models/Recipes.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import { Badge } from 'react-bootstrap';
+import { BrowserRouter as Router, Link, useLocation } from 'react-router-dom';
+import './Models.css'
+
+function RecipeListItem(props) {
+  const location = useLocation();
+  console.log(location);
+
+  // <Badge style={{backgroundColor: '#c990f0', color: 'black'}} bg=''>🐟 Seafood</Badge>{' '}
+  return (
+    <div className='modelListItem'>
+      <div className='modelListImage'></div>
+      <div className='modalListBadges'>
+        <Badge style={{backgroundColor: '#f58c8c', color: 'black'}} bg=''>🌎 American</Badge>{' '}
+        <Badge style={{backgroundColor: '#ffaa2b', color: 'black'}} bg=''> 🔥 Spicy</Badge>{' '}
+        <Badge style={{backgroundColor: '#cccccc', color: 'black'}} bg=''> ⌛ Quick</Badge>{' '}
+      </div>
+      <div className='modelListHeader'>{props.name}</div>
+      <Link to={location.pathname + '/' + props.link}>
+        <button className='modelListButton'></button>
+      </Link>
+    </div>
+  )
+}
 
 function Recipes() {
+    const recipes = Array(5).fill('Fish Filet');
+
+    const recipeElements = recipes.map((item, index) => 
+      <RecipeListItem name={item} link={index} />
+    )
+
     return (
-      <div className="recipes">Recipes</div>
+      <>
+        <div className='recipesTitle'>Recipes</div>
+        <div className='recipesList'>{recipeElements}</div>
+      </>
     );
   }
   
