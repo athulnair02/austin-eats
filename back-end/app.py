ceb3a333e3171251d732466b1d78279d457a5f22
Mihika Birmiwal <mihikabirmiwal@Parags-MacBook-Air.local>
2022-10-13 15:44:13 -0500

    added framework for our own api endpoints

--- back-end/app.py
+++ back-end/app.py
@@ -10,3 +10,26 @@ db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run()
+
+@app.route('/cultures', methods=["GET"])
+def getCultures():
+
+@app.route('/cultures/id=<id>', methods=["GET"])
+def getCulture_id(id):
+    culture = db.session.query(Culture).filter_by(id=id)
+
+
+@app.route('/recipes', methods=["GET"])
+def getRecipes():
+
+@app.route('/recipes/id=<id>', methods=["GET"])
+def getRecipe_id(id):
+    recipe = db.session.query(Recipe).filter_by(id=id)
+
+
+@app.route('/restaurants', methods=["GET"])
+def getRestaurants():
+
+@app.route('/restaurants/id=<id>', methods=["GET"])
+def getRestaurant_id(id):
+    recipe = db.session.query(Restaurant).filter_by(id=id)
