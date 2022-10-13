b424e8f7267b31d6ea2a73756a0f66d903a34058
Thomas Moore <tmooretcm@utexas.edu>
2022-10-13 18:50:20 -0500

    Setup for scraping; have to define model attributes and scrape from API

--- back-end/models.py
+++ back-end/models.py
@@ -1,5 +1,15 @@
-from app import db
+from flask import Flask, make_response, jsonify
+from db import init_db
+from flask_cors import CORS
+from flask_marshmallow import Marshmallow
+from marshmallow import fields, post_dump
 
+app = Flask(__name__)
+CORS(app)
+db = init_db(app)
+ma = Marshmallow(app)
+
+# Association table between cultures and restaurants; many to many relationship
 join_culture_restaurant = db.Table(
     "join_culture_restaurant",
     db.Column(
@@ -10,6 +20,7 @@ join_culture_restaurant = db.Table(
     ),
 )
 
+# Association table between cultures and recipes; many to many relationship
 join_culture_recipe = db.Table(
     "join_culture_recipe",
     db.Column(
@@ -20,20 +31,40 @@ join_culture_recipe = db.Table(
     ),
 )
 
+# Association table between restaurants and recipes; many to many relationship
+join_restaurant_recipe = db.Table(
+    "join_restaurant_recipe",
+    db.Column(
+        "restaurant_id", db.Integer, db.ForeignKey("restaurants.id"), primary_key=True
+    ),
+    db.Column(
+        "recipe_id", db.Integer, db.ForeignKey("recipes.id"), primary_key=True
+    ),
+)
+
 class Culture(db.Model) :
     __tablename__ = 'cultures'
-    id = db.Column(db.Integer, primaryKey=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
     # fill out rest of culture attributes here
+
+    def __repr__(self):
+        return "<Culture %s>" % self.name
 
 
 class Restaurant(db.Model) :
     __tablename__ = 'restaurants'
-    id = db.Column(db.Integer, primaryKey=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
+
+    def __repr__(self):
+        return "<Restaurant %s>" % self.name
 
 
 class Recipe(db.Model) :
     __tablename__ = 'recipes'
-    id = db.Column(db.Integer, primaryKey=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
+
+    def __repr__(self):
+        return "<Recipe %s>" % self.name
