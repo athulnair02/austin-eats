a51ad8eff95bdd621f2e1ee0cee087eec4bb3f7e
Mihika Birmiwal <mihikabirmiwal@Parags-MacBook-Air.local>
2022-10-16 18:35:04 -0500

    made models in models.py

--- back-end/models.py
+++ back-end/models.py
@@ -46,7 +46,16 @@ class Culture(db.Model) :
     __tablename__ = 'cultures'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
-    # fill out rest of culture attributes here
+    capital = db.Column(db.String())
+    flags = db.Column(db.BLOB)
+    currencies = db.Column(db.PickleType)
+    languages = db.Column(db.PickleType)
+    subregion = db.Column(db.String())
+    region = db.Column(db.String())
+    population = db.Column(db.Integer)
+    demonym = db.Column(db.String())
+    independent = db.Column(db.Boolean)
+    summary = db.Column(db.String())
 
     def __repr__(self):
         return "<Culture %s>" % self.name
@@ -56,6 +65,18 @@ class Restaurant(db.Model) :
     __tablename__ = 'restaurants'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
+    image_url = db.Column(db.String())
+    url = db.Column(db.String())
+    display_phone = db.Column(db.String())
+    rating = db.Column(db.Float)
+    review_count = db.Column(db.Integer)
+    display_address = db.Column(db.String())
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    photos = db.Column(db.BLOB)
+    price = db.Column(db.Integer)
+    delivery = db.Column(db.Boolean)
+    hours = db.Column(db.String())
 
     def __repr__(self):
         return "<Restaurant %s>" % self.name
@@ -65,6 +86,18 @@ class Recipe(db.Model) :
     __tablename__ = 'recipes'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
+    summary = db.Column(db.String())
+    image = db.Column(db.BLOB)
+    source = db.Column(db.String())
+    ready_in_minutes = db.Column(db.Integer)
+    servings = db.Column(db.Float)
+    labels = db.Column(db.PickleType)
+    health_score = db.Column(db.Float)
+    ingredients = db.Column(db.PickleType)
+    nutrition = db.Column(db.String())
+    instructions = db.Column(db.String())
+    cuisines = db.Column(db.PickleType)
+    dish_types = db.Column(db.PickleType)
 
     def __repr__(self):
         return "<Recipe %s>" % self.name
