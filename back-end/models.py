f3d88b65c674d1844bf8b90fb18e4b9813b242ac
Thomas Moore <tmooretcm@utexas.edu>
2022-10-16 18:35:55 -0500

    Model schemas

--- back-end/models.py
+++ back-end/models.py
@@ -68,3 +68,74 @@ class Recipe(db.Model) :
 
     def __repr__(self):
         return "<Recipe %s>" % self.name
+
+class CultureSchema() :
+    id = fields.Integer(required=True)
+    name = fields.String(required=True)
+
+    restaurants = fields.Nested("RestaurantSchema", only=("id", "name", "image", "rating", "review_count", "price"), required=True, attribute="restaurants", many=True)
+    recipes = fields.Nested("RecipeSchema", only=("id", "name", "summary", "meal_type", "dish_type", "ready_in_minutes", "servings"), required=True, attribute="recipes", many=True)
+
+    capital = fields.String(required=True)
+    flags = fields.List(fields.String(), required=True)
+    currencies = fields.Dict(keys=fields.String(), values=fields.String(), required=True)
+    languages = fields.Dict(keys=fields.String(), values=fields.String(), required=True)
+    subregion = fields.String(required=True)
+    region = fields.String(required=True)
+    population = fields.Integer(required=True)
+    latlng = fields.List(fields.Integer(), required=True)
+    demonym = fields.String(required=True)
+    independent = fields.Boolean(required=True)
+    summary = fields.String(required=True)
+
+# may need to adjust this MenuSchema
+class MenuSchema() :
+    id = fields.Integer(required=True)
+    dishes = fields.List(fields.String(), required=True)
+
+class RestaurantSchema() :
+    id = fields.Integer(required=True)
+    name = fields.String(required=True)
+
+    dishes = fields.Pluck(MenuSchema, "dishes", many=True)
+    cultures = fields.Nested("CultureSchema", only=("id", "name", "demonym", "region"), required=True, attribute="cultures", many=True)
+    # not sure how to link recipes to the dishes; we will see!
+    recipes = fields.Nested("RecipeSchema", only=("id", "name", "summary", "meal_type", "dish_type", "ready_in_minutes", "servings"), required=True, attribute="recipes", many=True)
+
+    image = fields.String(required=False, attribute="image_url")
+    url = fields.String(required=False, attribute="restaurant_url")
+    display_phone = fields.String(required=True)
+    categories = fields.List(fields.Dict(keys=fields.String(), values=fields.String(), required=True), required=True)
+    rating = fields.Integer(required=True)
+    review_count = fields.Integer(required=True)
+    location = fields.List(fields.String(), required=True, attribute="display_address")
+    latlng = fields.Dict(keys=fields.String(), values=fields.Integer(), required=True)
+    price = fields.String(required=True)
+    # takeout, delivery
+    transactions = fields.List(fields.String(), required=True)
+
+class RecipeSchema() :
+    id = fields.Integer(required=True)
+    name = fields.String(required=True, attribute="label")
+
+    restaurants = fields.Nested("RestaurantSchema", only=("id", "name", "image", "rating", "review_count", "price"), required=True, attribute="restaurants", many=True)
+    cultures = fields.Nested("CultureSchema", only=("id", "name", "demonym", "region"), required=True, attribute="cultures", many=True)
+
+    uri = fields.String(required=True, attribute="recipe_uri")
+    summary = fields.String(required=True)
+    image = fields.String(required=False, attribute="image_url")
+    source = fields.String(required=True)
+    source_url = fields.String(required=True, attribute="source_url")
+    ready_in_minutes = fields.Integer(required=True, attribute="totalTime")
+    servings = fields.Integer(required=True, attribute="yield")
+    # low-fat, etc
+    diet_labels = fields.List(fields.String(), required=True)
+    # sugar-free, keto, etc.
+    health_labels = fields.List(fields.String(), required=True)
+    ingredients = fields.List(fields.Dict(keys=fields.String(), values=fields.String(), required=True), required=True)
+    total_nutrients = fields.Dict(keys=fields.String(), values=fields.Dict(), required=True)
+    cuisine_type = fields.List(fields.String(), required=True)
+    # b/l/d
+    meal_type = fields.List(fields.String(), required=True)
+    # bread, etc.
+    dish_type = fields.List(fields.String(), required=True)
