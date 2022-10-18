126afe32cd6f06b95822d3f75a19f82e876075f8
babymariobebe <christiandcamp@hotmail.com>
2022-10-18 18:44:25 -0500

    started populate recipe

--- back-end/populate_db.py
+++ back-end/populate_db.py
@@ -1,4 +1,11 @@
 # populating the database from the json data
+import json
+from pydoc import doc
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# JSON paths
+RECIPE_JSON = "./api/ScrapeJSONs/Chownow.json"
 
 def populate_restaurants():
     engine = create_engine('postgresql://FindFootprints:FindThoseFootprints1@findingfootprints-db.cyocgz8iqxu8.us-east-1.rds.amazonaws.com:5432/postgres')
@@ -8,5 +15,13 @@ def populate_restaurants():
 
 
 def populate_recipes():
+    # todo: Set up key stuff & db
+
+    with open(RECIPE_JSON) as recipe_json:
+        recipe_data = json.load(recipe_json)
+
+        for recipe in recipe_data:
+            # todo: Populate data yeaaaa
+
 
 def populate_cultures():
