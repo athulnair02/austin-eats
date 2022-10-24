0c12cc6e3e900955c393dd9e3ff2e6d375275b2d
Thomas Moore <tmooretcm@utexas.edu>
2022-10-23 23:10:51 -0500

    All models pushed

--- back-end/db.py
+++ back-end/db.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+# TexasVotes code helped a lot with this
+
 def init_db(app):
     # need to set up AWS DB before we can have the key
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("AWS_DB_KEY")
--- back-end/models.py
+++ back-end/models.py
@@ -1,9 +1,11 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask
 from db import init_db
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
-from marshmallow import fields, post_dump
+from marshmallow import fields
 from sqlalchemy.dialects.postgresql import ARRAY
+
+# TexasVotes code helped a lot with this
 
 app = Flask(__name__)
 CORS(app)
