5ea12f5af23d68c9c1284cbe32adc421d0c8ec77
babymariobebe <christiandcamp@hotmail.com>
2022-10-26 19:28:19 -0500

    New sort/filter components, Pagination

--- front-end/src/components/Models/Cultures.js
+++ front-end/src/components/Models/Cultures.js
@@ -1,18 +1,80 @@
 import React from 'react';
 import cultures from '../../temp-backend/cultures.json'
 import { Create_Culture_Cell } from '../../SharedFunctions';
+import Selection from './sub_components/Selection';
+import InputField from './sub_components/InputField';
+import PaginateTable from './sub_components/PaginateTable';
 import { Row, Dropdown, Form} from "react-bootstrap";
+import { Stack } from '@mui/system';
+import { Divider } from '@mui/material';
 import '../../styles/Models.css'
 
 function Cultures() {
-    const cultureElements = cultures.map((culture, index) => 
-      Create_Culture_Cell(culture, index)
-    )
+    const columns = React.useMemo(() => [
+      {
+        Header: "Id",
+        accessor: "id",
+      },
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Region",
+        accessor: "region",
+      },
+      {
+        Header: "Subregion",
+        accessor: "subregion",
+      },
+      {
+        Header: "Population",
+        accessor: "population",
+      },
+      {
+        Header: "Independent",
+        accessor: "independent",
+      },
+    ], []);
+
+    const model_data = cultures;
+    const data = React.useMemo(() => {
+      const t = [];
+      for (const [id, culture] of Object.entries(model_data)) {
+        t.push({
+          id: id,
+          name: culture.name,
+          region: culture.region,
+          subregion: culture.subregion,
+          population: culture.population,
+          independent: culture.independent,
+        });
+      }
+      return t;
+    }, [])
 
     return (
-      <React.Fragment>
-        <div className='recipesTitle'>Cultures</div>
-        <div class = "cultures-information">
+      <>
+        <div className='modelTitle'>Cultures</div>
+        <Stack
+          direction="row"
+          divider={<Divider orientation="vertical" flexItem />}
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+          className='modelFilterBar'
+        >
+          <Selection text='Independence' helpText='Filter by independence status' choices={['Independent', 'Not independent']}></Selection>
+          <InputField helpText='Filter by min population' unit={<b>{'>'}</b>} unitPosition='start'></InputField>
+          <InputField helpText='Search for region'></InputField>
+          <InputField helpText='Search for subregion'></InputField>
+          <InputField helpText='Filter by min number of blocs' unit='blocs' unitPosition='end'></InputField>
+        </Stack>
+        <PaginateTable columns={columns} data={data} create_cell={(id) => {
+          return Create_Culture_Cell(model_data[id], id);
+        }}/>
+
+        {/* <div class = "cultures-information">
           <Row>
             <Dropdown class="dropdownStyle">
               <Dropdown.Toggle variant="success" id="dropdown-basic">
@@ -40,9 +102,8 @@ function Cultures() {
               <Form.Control type="regionalBlocs" placeholder="# of regional blocs" />
             </Form.Group>
           </Row>
-        </div>
-        <div className='recipesList'>{cultureElements}</div>
-      </React.Fragment>
+        </div> */}
+      </>
     );
   }
   
