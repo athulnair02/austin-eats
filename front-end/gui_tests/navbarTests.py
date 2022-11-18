20a9c4d6b48b272559d15a7a794df2bf90875d64
Athul Nair <athulnair02@gmail.com>
2022-11-17 18:36:36 -0600

    fixed selenium tests for ci using v3

--- front-end/gui_tests/navbarTests.py
+++ front-end/gui_tests/navbarTests.py
@@ -6,7 +6,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import sys
@@ -28,7 +27,7 @@ class TestNavbar(unittest.TestCase):
         ops.add_argument("--no-sandbox")
         ops.add_argument("--disable-dev-shm-usage")
 
-        cls.driver = webdriver.Chrome(service=Service(PATH), options=ops)
+        cls.driver = webdriver.Chrome(PATH, options=ops)
         cls.driver.get(URL)
 
     # Close browser and quit after all tests
